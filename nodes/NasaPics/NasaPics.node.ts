e7a6e8ac14192f499e7a784edf5e09c6777aa077
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-04 11:59:41 +0530

    Step 4.2: Create main class

--- nodes/NasaPics/NasaPics.node.ts
+++ nodes/NasaPics/NasaPics.node.ts
@@ -1,2 +1,10 @@
 import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 
+export class NasaPics implements INodeType {
+	description: INodeTypeDescription = {
+		// Basic node details go here
+		properties: [
+			// Resources and operations go here
+		],
+	};
+}
