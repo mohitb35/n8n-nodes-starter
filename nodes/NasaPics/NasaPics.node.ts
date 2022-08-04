5975f4c74fe1f5a20c2af5c965631f596a62d379
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2022-08-04 12:00:09 +0530

    Step 4.3: Add node details (description)

--- nodes/NasaPics/NasaPics.node.ts
+++ nodes/NasaPics/NasaPics.node.ts
@@ -3,6 +3,31 @@ import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 export class NasaPics implements INodeType {
 	description: INodeTypeDescription = {
 		// Basic node details go here
+		displayName: 'NASA Pics',
+		name: 'NasaPics',
+		icon: 'file:nasapics.svg',
+		group: ['transform'], //transform currently has no effect
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Get data from NASAs API',
+		defaults: {
+			name: 'NASA Pics',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'NasaPicsApi', //must match name property in credential file
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: 'https://api.nasa.gov',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
 		properties: [
 			// Resources and operations go here
 		],
